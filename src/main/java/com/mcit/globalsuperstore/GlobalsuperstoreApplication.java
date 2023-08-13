813d03e3b5758388f8564c7474812f0df70e0a24
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2023-08-13 09:29:29 +0430

    thymeleaf: Order and inventory project

---
+++ src/main/java/com/mcit/globalsuperstore/GlobalsuperstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mcit.globalsuperstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalsuperstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalsuperstoreApplication.class, args);
+	}
+
+}
